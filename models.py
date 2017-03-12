faf4d67a520a9901a0fb3d8f0cb1066a4bf73bd8
David Ton-Lai <dtonlai@ualberta.ca>
2017-03-12 19:49:31 +0900

    Create User model

--- models.py
+++ models.py
@@ -1,1 +1,20 @@
 from peewee import *
+import datetime
+
+DATABASE = SqliteDatabase("social.db")
+
+
+class User(Model):
+	username = CharField(unique=True)
+	email = CharField(unique=True)
+	password = CharField(max_length = 100)
+	joinDate = DateTImeField(default=datetime.datetime.now)
+	isAdmin = BooleanField(default=False)
+
+	class Meta:
+		database = DATABASE
+
+if __name__ = "__main__":
+	DATABASE.connect()
+	DATABASE.create_tables([User], safe=True)
+	order_by("--joinDate",)
